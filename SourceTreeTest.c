3398781ce4b3d6b5323fda483583db423d9e60d0
GAUDUL <limjsu12@naver.com>
2023-11-01 17:25:41 +0900

    Test 브랜치 추가

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,15 @@
 
 int main(){
 
+    int i=0;
+
     printf("First OSS Code\n");
     printf("Second OSS Code");
     printf("마스터 브랜치 추가");
+
+    for(i=0;i<10;i++){
+        printf("Test 브랜치\n");
+    }
+    
     return 0;
 }
